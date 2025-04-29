92fcb19b8603eb67a680c02c2685dfb8dad886d7
Coding-JAG <124489740+Coding-JAG@users.noreply.github.com>
2025-04-29 20:52:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.querySelector('.image-wrapper').addEventListener('click', () => {
-    const text = document.querySelector('.image-wrapper').dataset.text;
-    navigator.clipboard.writeText(text)
-      .then(() => alert(`Copied: "${text}"`))
-      .catch(err => alert("Failed to copy text."));
-  });
-  
+  const text = document.querySelector('.image-wrapper').dataset.text;
+  navigator.clipboard.writeText(text)
+    .then(() => alert('Prompt copied to clipboard!'))
+    .catch(() => alert('Failed to copy the prompt.'));
+});
